2003f6f843330e7221634c0544e9fedaec0e64a3
Bokus <bokus@tut.by>
2022-05-21 00:34:25 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -22,3 +22,29 @@ for question in questions:
         print("Ответ верный")
     else:
         print("Ответ не верный")
+        
+--------------------------------------------------
+
+questions = {
+    'Что такое KPI?': {
+        'Показатель достижения успеха' : 'Верно',
+        'Рейтинг зарплат сотрудников' : 'Неверно',
+        'Список директоров компании' : 'Неверно',
+        'Календарь отпусков' : 'Неверно'
+    }
+}
+
+print('Для окончания онбординга пройдите тестирование:')
+for question in questions:
+    print(question)
+    n = 1
+    for answer in questions[question]:
+        print(n, answer)
+        if questions[question][answer] == 'Верно':
+            correct_answer = n
+
+    reply = int(input("Номер ответа:"))
+    if reply == correct_answer:
+        print("Ответ верный")
+    else:
+        print("Ответ не верный")
